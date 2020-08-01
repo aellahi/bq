f704b24f27ef5051d755f477a15868c9279aec5e
Aisha Ellahi <aishaellahi85@gmail.com>
2020-07-31 23:00:39 -0700

    mv files

---
+++ bq/add_new_record.py
@@ -0,0 +1,44 @@
+# Author: Aisha Ellahi
+
+import pandas as pd
+import yaml
+import sys
+
+FILENAME = sys.argv[1]
+NEW_RECORD = sys.argv[2]
+
+# Read run records
+records_df = pd.read_csv(FILENAME)
+records_df.set_index(['date', 'run'], inplace=True)
+records = dict(records_df)
+
+# read yaml
+with open(NEW_RECORD) as f:
+    new_record = yaml.load(f, Loader=yaml.FullLoader)
+
+# Lookup new entry index
+RECORD_IDX = new_record['date'], new_record['run']
+
+# mark run as completed
+records['completed'][RECORD_IDX] = True
+
+# Add new fields
+for col, field in new_record.items():
+
+    if (col == 'date') or (col == 'run'):
+        pass
+    else:
+        # TODO: Fix AssertionError (mismatch in types)
+        try:
+            records[col][RECORD_IDX] = field
+        except AssertionError:
+            print(col)
+            print(field)
+            print(" ")
+
+# re-create records df
+modified_records_df = pd.DataFrame(records)
+modified_records_df.reset_index(inplace=True, drop=True)
+
+# save
+modified_records_df.to_csv(FILENAME, index=False)
